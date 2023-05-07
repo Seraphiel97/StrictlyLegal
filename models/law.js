d71dc8a7db34c7c6170db774d94b816a4b052d6f
Seraphiel97 <seth.graves@outlook.com>
2023-05-07 19:25:24 -0400

    complete models

---
+++ models/law.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const lawSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true,
+    },
+    state: {
+        type: Schema.Types.ObjectId,
+        ref: 'State',
+        required: true,
+    },
+    question: {
+        type: String,
+        maxLength: 150,
+        required: true,
+    },
+    answer: {
+        type: String,
+        maxLength: 150,
+        required: true,
+    },
+    penalty: {
+        type: String,
+        maxLength: 150,
+    },
+    reference: {
+        type: String,
+    },
+    verification: {
+        type: Boolean,
+        default: false,
+    }
+})
---
+++ models/state.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const stateSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    population: {
+        type: Number,
+        required: true,
+    }
+})
