31e24ded4b2bba5cbb008f9779f831bf7163235b
Seraphiel97 <seth.graves@outlook.com>
2023-05-09 19:57:03 -0400

    fix bug with the lawList page for visitors

--- src/components/LawDisplay/LawDisplay.jsx
+++ src/components/LawDisplay/LawDisplay.jsx
@@ -15,26 +15,45 @@ export default function LawDisplay({law, user}) {
   
   return (
     <div>
-        <h3>{law.category.name}</h3>
-        <h3>{law.state.name}</h3>
-        <h5>{law.question}</h5>
-        <p>{law.answer}</p>
-        <p>{law.penalty}</p>
-        <p><a href={law.reference}>Further Information</a></p>
-        {user.isAdmin || user._id === law.user ?
-        <div>
-          <Link to={'/laws/' + law._id} state={law}>Update</Link>
-          <button onClick={() => handleDelete(law)}>Delete</button>
-        </div>
+        {user ?
+        <>
+          <h3>{law.category.name}</h3>
+          <h3>{law.state.name}</h3>
+          <h5>{law.question}</h5>
+          <p>{law.answer}</p>
+          <p>{law.penalty}</p>
+          <p><a href={law.reference}>Further Information</a></p>
+          {user.isAdmin || user._id === law.user ?
+          <div>
+            <Link to={'/laws/' + law._id} state={law}>Update</Link>
+            <button onClick={() => handleDelete(law)}>Delete</button>
+          </div>
+          :
+          <p></p>
+          }
+          {law.verification ?
+          <p>Verified</p>
+          :
+          <p>Awaiting Verification</p>
+          }
+          <hr />
+        </>
         :
-        <p></p>
-        }
-        {law.verification ?
-        <p>Verified</p>
-        :
-        <p>Awaiting Verification</p>
-        }
-        <hr />
+        <>
+          <h3>{law.category.name}</h3>
+          <h3>{law.state.name}</h3>
+          <h5>{law.question}</h5>
+          <p>{law.answer}</p>
+          <p>{law.penalty}</p>
+          <p><a href={law.reference}>Further Information</a></p>
+          {law.verification ?
+            <p>Verified</p>
+          :
+            <p>Awaiting Verification</p>
+          }
+          <hr />
+        </>
+        } 
     </div>
   )
 }
