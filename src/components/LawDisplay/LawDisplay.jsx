9aa829c729ac5e6c390ac8a4f11137b3b8c049a6
Seraphiel97 <seth.graves@outlook.com>
2023-05-15 09:14:17 -0400

    update styling on lawList page

--- src/components/LawDisplay/LawDisplay.jsx
+++ src/components/LawDisplay/LawDisplay.jsx
@@ -21,7 +21,11 @@ export default function LawDisplay({law, user, getLaws}) {
           <h5 className='font-text text-xl'>Q: {law.question}</h5>
           <p className='font-text text-xl'>A: {law.answer}</p>
           <p className='font-text text-lg'>{law.penalty}</p>
-          <p className='font-text text-lg'><a href={law.reference}>Further Information</a></p>
+          {law.reference != '' ?
+          <p className='font-text text-lg mb-2'>Click <a href={law.reference}>HERE</a> For Further Information</p>
+          :
+          <p className='font-text text-lg mb-2'>No reference provided</p>
+          }
           {user.isAdmin || user._id === law.user ?
           <div>
             <Link to={'/laws/' + law._id} state={law} className='px-2 py-1 w-20 h-10 text-lightGreen rounded-xl bg-charcoal hover:bg-white'>Update</Link>
@@ -43,7 +47,11 @@ export default function LawDisplay({law, user, getLaws}) {
           <h5 className='font-text text-xl'>Q: {law.question}</h5>
           <p className='font-text text-xl'>A: {law.answer}</p>
           <p className='font-text text-lg'>{law.penalty}</p>
-          <p className='font-text text-lg'>Click <a href={law.reference}>HERE</a> For Further Information</p>
+          {law.reference != '' ?
+          <p className='font-text text-lg mb-2'>Click <a href={law.reference}>HERE</a> For Further Information</p>
+          :
+          <p className='font-text text-lg mb-2'>No reference provided</p>
+          }
           {law.verification ?
             <p className='font-text text-lg'>Verified</p>
           :
